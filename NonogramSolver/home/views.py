b37eb0cf647ac4684d4aae563a95c33a2bb8a7ee
sohy19 <sohy19@hufs.ac.kr>
2022-05-27 12:57:25 +0900

    힌트 입력란 수정

--- NonogramSolver/home/views.py
+++ NonogramSolver/home/views.py
@@ -10,5 +10,13 @@ def next(request):
     col = request.GET['col']
     return render(request, 'next.html', {'row': row, 'col': col})
 
-def result(request):
-    return render(request, 'result.html')
+def result(request, row, col):
+    row_hints = []
+    col_hints = []
+    for i in range(1, row+1):
+        idx = "row-hint" + str(i)
+        row_hints.append(request.POST.get(idx))
+    for i in range(1, col+1):
+        idx = "col-hint" + str(i)
+        col_hints.append(request.POST.get(idx))
+    return render(request, 'result.html', {'row': row, 'col': col, 'row_hints': row_hints, 'col_hints': col_hints})
