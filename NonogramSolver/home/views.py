1e417d66ef881991c1a92fd0a8e794792094e3a1
sohy19 <sohy19@hufs.ac.kr>
2022-05-29 23:44:15 +0900

    css 수정

--- NonogramSolver/home/views.py
+++ NonogramSolver/home/views.py
@@ -14,9 +14,10 @@ def result(request, row, col):
     row_hints = []
     col_hints = []
     for i in range(1, row+1):
-        idx = "row-hint" + str(i)
+        idx = f"row-hint{i}"
         row_hints.append(request.POST.get(idx))
     for i in range(1, col+1):
         idx = "col-hint" + str(i)
         col_hints.append(request.POST.get(idx))
+    print(row_hints)
     return render(request, 'result.html', {'row': row, 'col': col, 'row_hints': row_hints, 'col_hints': col_hints})
